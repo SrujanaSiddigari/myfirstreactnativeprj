33a630afd78fe792fbeac89b2a2735a46067761a
SrujanaSiddigari <siddigarisru@gmail.com>
2019-12-27 21:22:04 +0530

    new changes

--- login.js
+++ login.js
@@ -1,80 +1,73 @@
 import React, { Component } from 'react';
-import { AsyncStorage, StyleSheet, View, TextInput, Button, Image, Alert, Text } from 'react-native';
+import { AsyncStorage, StyleSheet, View, Keyboard, TextInput, Button, Image, Alert, Text } from 'react-native';
 import { DashboardPage } from './dashboard.js';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import Toast from 'react-native-toast-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import config from './config';
 // import Form from 'react-native-form-validator';
 export class LoginPage extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            TextInputValue:'',
-            Password:''
+            TextInputValue: '',
+            Password: ''
         }
     }
-    buttonClickListener = () =>{
-        const { TextInputValue } = this.state ;
-        const { Password } = this.state ;
+    buttonClickListener = () => {
+        const { TextInputValue } = this.state;
+        const { Password } = this.state;
         // Alert.alert(TextInputValue);
         // Alert.alert(Password);
-        // this.validate({
-        //     TextInputValue  : {email: true, required:true},
-        //     Password: { maxlength:10, required: true},
-        //   });
-         fetch(config.API_URL + 'user-login', {
+        if(TextInputValue == ""){
+            alert('Please enter email');
+        }
+        else if(Password == ""){
+            alert('Please enter password');
+        }
+        else{
+            // alert('success');
+        fetch(config.API_URL + 'user-login', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body:JSON.stringify({
+            body: JSON.stringify({
                 username: TextInputValue,
                 password: Password,
-                rememberme : false,
+                rememberme: false,
                 site_id: 1
-               })
-            }).then(response => response.json())
+            })
+        }).then(response => response.json())
             .then((responseJson) => {
-              if(responseJson.code == 200) {
-                AsyncStorage.setItem('user_id',responseJson.data);
-                this.props.navigation.navigate('Dashboard');
-                const style={
-                    backgroundColor: "#4ADDFB",
-                    width: 300,
-                    color: "#ffffff",
-                    fontSize: 15,
-                    lineHeight: 2,
-                    lines: 4,
-                    borderRadius: 15,
-                    fontWeight: "bold",
-                    yOffset: 40
-                };
-                Toast.show('Welcome you have sucessfully logged in',Toast.LONG,Toast.TOP,style);
-              } else {
-                alert(JSON.stringify(responseJson.message));
-              }
+                if (responseJson.code == 200) {
+                    // alert('200');
+                    AsyncStorage.setItem('user_id', responseJson.data);
+                    this.props.navigation.navigate('Dashboard');
+                } else {
+                    alert(JSON.stringify(responseJson.message));
+                }
             })
             .catch((error) => {
-              alert(error); 
+                alert(error);
+                // console.log(error);
             })
+        }
+        Keyboard.dismiss();
     }
-    // _onsubmit(){
-    //     this.validate({
-    //         email:{reqiured:true,email:true},
-    //         password:{minlength:9,reqiured:true}
-    //     });
-    // }
+
     render() {
         return (
             <View style={styles.container}>
                 <Image style={styles.imgstyl} source={require('./images/repdonkey-2-full-logo.png')} />
-                <TextInput style={styles.textinput} onChangeText={TextInputValue => this.setState({TextInputValue})} placeholder = 'enter email address'></TextInput>
-                <TextInput style={styles.textinput} onChangeText={Password => this.setState({Password})} secureTextEntry={true} placeholder = 'enter password'></TextInput>
-                <Button title="sign in" onPress={(this.buttonClickListener.bind(this))} />
+                <TextInput style={styles.textinput} onChangeText={TextInputValue => this.setState({ TextInputValue })} placeholder='enter email address'></TextInput>
+                <TextInput style={styles.textinput} onChangeText={Password => this.setState({ Password })} secureTextEntry={true} placeholder='enter password'>
+                </TextInput>
+                <Button title="sign in" onPress={(this.buttonClickListener)}/>
                 <Text style={styles.footercontent}>
-                Copyright © 2019 Repdonkey Portal. {"\n"}All rights reserved.
+                    Copyright © 2019 Repdonkey Portal. {"\n"}All rights reserved.
                 </Text>
             </View>
         );
@@ -91,6 +84,7 @@ export class LoginPage extends Component {
     //     // }
     // }
 }
+ 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -105,7 +99,8 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         width: 400,
         height: 50,
-        marginBottom: 15
+        marginBottom: 15,
+        borderRadius:5
     },
     btnstyl: {
         marginTop: 20,
@@ -116,17 +111,17 @@ const styles = StyleSheet.create({
         height: 95,
         marginBottom: 35
     },
-    footercontent:{
-        marginTop:20,
-        justifyContent:'center',
-        alignContent:'center',
-        fontSize:19,
-        textAlign:'center'
+    footercontent: {
+        marginTop: 20,
+        justifyContent: 'center',
+        alignContent: 'center',
+        fontSize: 19,
+        textAlign: 'center'
     }
 });
 const loginContainer = createStackNavigator({
-    Home : LoginPage,
+    Home: LoginPage,
     Dashboard: DashboardPage
-    });
-    const Navigatorr = createAppContainer(loginContainer);
-    export default Navigatorr;
+});
+const Navigatorr = createAppContainer(loginContainer);
+export default Navigatorr;
